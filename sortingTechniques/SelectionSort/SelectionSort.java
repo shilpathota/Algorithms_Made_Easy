471f19990566c7c8705f8757dcb327cf3e2991d3
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-11-20 15:24:00 -0600

    Create SelectionSort.java

---
+++ sortingTechniques/SelectionSort/SelectionSort.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    static void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public static void selectionSort(int[] arr){
+        long startTime = System.nanoTime();
+
+        for(int i=0;i<arr.length;i++){
+            int min_index = i;
+            for(int j=i+1;j<arr.length-1;j++){
+                if(arr[min_index]>arr[j]){
+                    min_index=j;
+                }
+            }
+            swap(arr,min_index,i);
+        }
+        long stopTime = System.nanoTime();
+        System.out.println("Time Taken to execute:"+
+                (stopTime - startTime) +" milliseconds");
+        System.out.println("After Sorting: "+Arrays.toString(arr));
+    }
+}
