7e44067b46f442de858b0f8371efed936503732d
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-11-20 15:51:17 -0600

    Update SelectionSort.java

--- sortingTechniques/SelectionSort/SelectionSort.java
+++ sortingTechniques/SelectionSort/SelectionSort.java
@@ -25,4 +25,31 @@ public class SelectionSort {
                 (stopTime - startTime) +" milliseconds");
         System.out.println("After Sorting: "+Arrays.toString(arr));
     }
+    public static void optimizedselectionSort(int[] arr){
+        long startTime = System.nanoTime();
+
+        for(int i=0;i<arr.length;i++){
+            int min_index = i;
+            int max_index=i;
+            for(int j=i+1;j<arr.length-1;j++){
+                if(arr[min_index]>arr[j]){
+                    min_index=j;
+                }
+                if(arr[max_index]<arr[j]){
+                    max_index=j;
+                }
+            }
+            swap(arr,min_index,i);
+
+            if(arr[min_index]==arr[max_index]){
+                swap(arr,arr.length-1,min_index);
+            }else{
+                swap(arr,arr.length-1,max_index);
+            }
+        }
+        long stopTime = System.nanoTime();
+        System.out.println("Time Taken to execute:"+
+                (stopTime - startTime) +" milliseconds");
+        System.out.println("After Sorting: "+Arrays.toString(arr));
+    }
 }
