8a23cbc1a040b752b721a7715c287323b7dbfa15
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-11-20 14:49:54 -0600

    Create Main.java

---
+++ sortingTechniques/QuickSort/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static int[] generateRandomNumbers(int n){
+        int[] arr = new int[n];
+
+        // Create a Random object
+        Random random = new Random();
+
+        // Assign random values to the array
+        for (int i = 0; i < n; i++) {
+
+            // Generate a random integer
+            // between INT_MIN and INT_MAX
+            arr[i] = random.nextInt(1000);
+        }
+
+        // Print the array
+        System.out.println("Before Sorted:"+Arrays.toString(arr));
+        return arr;
+    }
+    //quicksort
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] arra=generateRandomNumbers(90);
+//        Arrays.sort(arra);
+        QuickSort.quickSortMain(arra);
+    }
+}
