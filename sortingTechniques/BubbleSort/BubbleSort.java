7fb87cb0dfd41b07a21431f80fd433d4680a9c41
shilpa <36531617+shilpathota@users.noreply.github.com>
2024-11-21 09:50:36 -0600

    Update BubbleSort.java

--- sortingTechniques/BubbleSort/BubbleSort.java
+++ sortingTechniques/BubbleSort/BubbleSort.java
@@ -1,36 +1,101 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.Random;
 
-public class Main {
-    public static int[] generateRandomNumbers(int n){
-        int[] arr = new int[n];
+public class BubbleSort {
+    public static void bubbleSort(int[] ele){
+        // 2 iterations
+        // 1 iteration the highest element is bubbled up to the last
+        // outer loop traverse through all elements
+        //Optimizations: 1. Remove the last element from the iteration as it is already palced 2 . if already sorted without swap,then loop stops
+        //BRuteForce
 
-        // Create a Random object
-        Random random = new Random();
-
-        // Assign random values to the array
-        for (int i = 0; i < n; i++) {
-
-            // Generate a random integer
-            // between INT_MIN and INT_MAX
-            arr[i] = random.nextInt(1000);
+        long startTime = System.nanoTime();
+        for(int i=0;i<ele.length;i++){
+            for(int j=i+1;j<ele.length;j++){
+                if(ele[i]>ele[j]){
+                    int temp = ele[i];
+                    ele[i]=ele[j];
+                    ele[j]=temp;
+                }
+            }
         }
+        long stopTime = System.nanoTime();
+        System.out.println("Time Taken to execute:"+
+                (stopTime - startTime) +" milliseconds");
 
-        // Print the array
-        System.out.println("Before Sorted:"+Arrays.toString(arr));
-        return arr;
+        System.out.println("After Sorted: "+Arrays.toString(ele));
     }
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void OptimizedbubbleSort(int[] ele){
+        // 2 iterations
+        // 1 iteration the highest element is bubbled up to the last
+        // outer loop traverse through all elements
+        //Optimizations: 1. Remove the last element from the iteration as it is already palced 2 . if already sorted without swap,then loop stops
+        //BRuteForce
+        Boolean swapped = false;
+        long startTime = System.nanoTime();
+        for(int i=0;i<ele.length;i++){
+            swapped = false;
+            for(int j=i+1;j<ele.length-i;j++){
+                if(ele[i]>ele[j]){
+                    int temp = ele[i];
+                    ele[i]=ele[j];
+                    ele[j]=temp;
+                    swapped=true;
+                }
 
-        int[] arra=generateRandomNumbers(90);
-        Arrays.sort(arra);
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        long stopTime = System.nanoTime();
+        System.out.println("Time Taken to execute:"+
+                (stopTime - startTime) +" milliseconds");
 
-        BubbleSort.bubbleSort(arra);
-        BubbleSort.OptimizedbubbleSort(arra);
-        BubbleSort.optimizedCodeBubbleSort(arra);
-        BubbleSort.bubbleSort_withFlag(arra);
+        System.out.println("After Sorted: "+Arrays.toString(ele));
+    }
+    public static int[] optimizedCodeBubbleSort(int[] input) {
+        long startTime = System.nanoTime();
+        boolean swapped = true;
+        for (int pass = input.length - 1; pass >= 0 && swapped; pass--) {
+            swapped = false;
+            for (int i = 0; i < pass; i++) {
+                if (input[i] > input[i + 1]) {
+                    int temp = input[i];
+                    input[i] = input[i + 1];
+                    input[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        }
+        System.out.println("Time taken for OPTIMIZED bubbleSort: "
+                + (System.nanoTime() - startTime));
+        return input;
+    }
+
+    public static void bubbleSort_withFlag(int[] intArr) {
+        long startTime = System.nanoTime();
+
+        int lastComparison = intArr.length - 1;
+
+        for (int i = 1; i < intArr.length; i++) {
+            boolean isSorted = true;
+            int currentSwap = -1;
+            for (int j = 0; j < lastComparison; j++) {
+                if (intArr[j] < intArr[j + 1]) {
+                    int tmp = intArr[j];
+                    intArr[j] = intArr[j + 1];
+                    intArr[j + 1] = tmp;
+                    isSorted = false;
+                    currentSwap = j;
+                }
+
+            }
+            if (isSorted) return;
+            lastComparison = currentSwap;
+        }
+        System.out.println("Time taken for with Flag bubbleSort: "
+                + (System.nanoTime() - startTime));
     }
 }
